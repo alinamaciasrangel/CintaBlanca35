f1a9c1a8f82f7e236a126fb933517f1f677d0fde
Alina Macias <fantascopia@gmail.com>
2019-10-28 21:52:53 -0600

    fatal git

---
+++ 04_javascript/main.js
@@ -0,0 +1,98 @@
+// /* /* //comprobar si esta bien linkeada
+// //prompt ("hola") */
+
+
+// //valor numerico
+// let number = 10
+
+// //valor string o texto
+// let texto = "alina";
+// let texto2 = "alina2"
+
+// //valor booleano  (true es 1) (false es 0)
+// let verdadero = true
+// let falso = false
+
+// // VARIABLES ESPECIALES
+
+// //objeto
+// let object = {}
+// //arreglo
+// let array = {}
+
+// //---------------------------------------->
+
+// let x = 100;
+// let y = 50;
+// let suma = x + y 
+// console.log(suma)
+
+// // + - * / %
+
+// let cambiar = 1500
+// console.log (cambiar)
+// cambiar = 10
+// console.log (cambiar)
+
+// //------------------------------------------->
+
+// let frutas = ["melon", "sandia", "fresa"]
+// frutas {3} = "manzana"
+// frutas.push ["guayaba"]
+// frutas.splice (1,2)
+// console.log (frutas)
+
+// //-------------------------------------------->
+
+// let persona = {
+//     nombre : "alina",
+//     direccion : "aragon",
+//     telefono : "123456789",
+//     edad : "alinamacias23@gmail.com",
+//     edad : "21",
+//     mascotas : {
+//         perritos : ["salchicha", 
+//          "french", "oso"],
+//     musica : ["electronica", "metal", "pop"]
+// }
+
+// console.log(persona.nombre)
+
+//----------------------------> */
+
+// prompt ("cómo te llamas");
+
+// let nombre = prompt ("cómo te llamas");
+// console.log ('Nombre ' + nombre);
+
+// prompt("cuántos años tienes");
+// let edad = prompt ("cuántos años tienes");
+// console.log ('Edad' + edad);
+
+// prompt ("donde vives");
+// let domicilio = prompt ("donde vives");
+// console.log ('Domicilio ' + domicilio);
+
+
+// let fecha = prompt ('ingresa tu fecha de nacimiento');
+
+// console.log(typeof(fecha))
+
+// let anio = Number(fecha)
+// let anioActual = 2019;
+
+// console.log (anioActual - anio);
+
+let persona = {
+
+    nombre : "alina",
+    edad : "27",
+    domicilio : "aragon",
+    telefono : "123456789",
+    peliculas : {"acción", "romance", "comedia", "terror"]
+    musica : {"rock", "pop", "español"}
+
+    console.log (persona)
+
+
+
